4a4f2b848e4a84ddbcd186c07de26178ce28b29e
selim2066 <mdselimreza2066@gmail.com>
2025-07-19 22:44:21 +0600

    starting

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './component/header/header'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
